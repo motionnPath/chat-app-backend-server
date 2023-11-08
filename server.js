bdf42f1497ff9f69aab8c4b380daccc15e5110f8
motionnPath <mathlouthi.91.oussama@gmail.com>
2023-11-08 16:28:15 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+
+
+require('dotenv').config();
+
+const app = express();
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  };
+//setting the middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+//starting connection to mongodb
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri,{ useNewUrlParser:true});
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log('MongoDB database connection established successfully');
+})
+
+//tell the server to use the files of mongoDB
+const userRouter = require('./routes/users')
+app.use('/users', userRouter);
+
+const conversationRouter = require('./routes/conversations')
+app.use('/conversations', conversationRouter);
+
+const server_port = process.env.SERVER_PORT || 4000;
+
+app.listen(server_port, ()=>{
+    console.log('Server is running on port: ',server_port)
+})
