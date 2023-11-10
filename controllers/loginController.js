c330a5c90b192acb59466d0fef362577d4353dc7
motionnPath <mathlouthi.91.oussama@gmail.com>
2023-11-10 18:02:34 +0100

    loggin tokens

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -36,7 +36,9 @@ const loginAuth = async (req, res) => {
       user.refreshToken = refreshToken;
       const result = await user.save();
       
-  
+      console.log('refrechToken = ', refreshToken)
+      console.log('accessToken = ', accessToken)
+        
       res.cookie('jwt',refreshToken, {httpOnly: true, maxAge: 24 * 60 * 60 * 1000}); // ms
       res.json({username: user.username ,accessToken});
       
