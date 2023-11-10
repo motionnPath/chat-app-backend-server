2d022b62c261311662d0aa43a9c60e8402162abf
motionnPath <mathlouthi.91.oussama@gmail.com>
2023-11-10 18:08:36 +0100

    logging cookies

--- controllers/refrechTokenController.js
+++ controllers/refrechTokenController.js
@@ -4,7 +4,9 @@ require('dotenv').config()
 
 const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
-    //console.log("recieved cookies on server refrechtoken", cookies)
+    
+    console.log("recieved cookies on server refrechtoken", cookies.jwt)
+    
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
 
