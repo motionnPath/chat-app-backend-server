e2ebf1affdae8dd34f4f7ee424fbc3f553e8a5c5
motionnPath <mathlouthi.91.oussama@gmail.com>
2023-11-12 16:59:19 +0100

    keys

--- controllers/subscribeToPushNotification.js
+++ controllers/subscribeToPushNotification.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 
 const vapidKeys = webpush.generateVAPIDKeys();
 
-const publicVapiKey = vapidKeys.publicKey;
+const publicVapiKey = "BOGHw5Hs6L97UJypiTH6T9XCcY6TOH9TZ9lwTSri-rmbGqzZWb7MHwnI0j3vVl0Njw7vUlEz7S0P9kP6JKsvxtU";
 
-const privateVapiKey = vapidKeys.privateKey;
+const privateVapiKey = "lYNaqhcRo3f20ievMiOV2aLWyAMiwEg4WnK-2pKQ1JU";
 
 
 webpush.setVapidDetails(
